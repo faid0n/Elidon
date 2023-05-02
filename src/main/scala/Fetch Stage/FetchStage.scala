140aaf82626762528279f64ba52cdea1b46a2758
Faidon Kourounakis <faidon.kourounakis@gmail.com>
2023-05-02 15:16:49 +0200

    removed vhdl

--- src/main/scala/Fetch Stage/FetchStage.scala
+++ src/main/scala/Fetch Stage/FetchStage.scala
@@ -4,5 +4,7 @@ import chisel3.util._
 class FetchStage extends Module {
   // val io = IO(new Bundle {
   // })
-  val instructionMemory = Module(new InstructionMemory)
+  // val pcReg = Vec(RegInit()
+
+  // val instructionMemory = Vec()
 }
--- src/main/scala/Fetch Stage/InstructionMemory.scala
+++ src/main/scala/Fetch Stage/InstructionMemory.scala
@@ -4,7 +4,7 @@ import chisel3.util._
 class InstructionMemory extends Module {
   // val io = IO(new Bundle {
   //   val pc = Input(UInt(8.W))
-  //   val o
+  //   val instruction = Output(UInt(16.W))
   // })
-  // val instructionMem = Module(new Memory)
+  // val instructionMem = Vec(16.W)
 }
